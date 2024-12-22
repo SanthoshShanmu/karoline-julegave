091234c6e882ba3edd6827eec74381402f481d41
Santhosh Shanmugam <santhosh.shanmugam@gjensidige.no>
2024-12-22 22:00:24 +0100

    ensure mobile support

--- theater-surprise/src/components/Announcement.tsx
+++ theater-surprise/src/components/Announcement.tsx
@@ -5,11 +5,15 @@ const Announcement = () => {
   return (
     <div className="announcement">
       <h1>🎭 Overraskelse! 🎭</h1>
-      <Carousel />
-      <div className="details">
-        <h2>Vi skal se musikalen "Karusell" på Nationaltheatret!</h2>
-        <p>16 Januar kl. 19.30 med date først</p>
-        <p>God Jul!🎄</p>
+      <div className="announcement-content">
+        <div className="carousel-column">
+          <Carousel />
+        </div>
+        <div className="details-column details">
+          <h2>Vi skal se musikalen "Karusell" på Nationaltheatret!</h2>
+          <p>16 Januar kl. 19.30 med date først</p>
+          <p>God Jul!🎄</p>
+        </div>
       </div>
     </div>
   )
